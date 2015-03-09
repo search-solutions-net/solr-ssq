1fbbe209676584c161979a81ca23d53eb0f94315
Paul-Blanchaert <paul@search-solutions.net>
2015-03-09 15:59:04 +0100

    Initial commit for 1.0.0

---
+++ src/main/java/net/sr_sl/solr/solr_ssq/SsqQueryComponent.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2015 search-solutions.net
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package net.sr_sl.solr.solr_ssq;
+
+import java.io.IOException;
+
+import org.apache.solr.common.params.CommonParams;
+import org.apache.solr.common.params.DisMaxParams;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.handler.component.QueryComponent;
+import org.apache.solr.handler.component.ResponseBuilder;
+import org.apache.solr.request.SolrQueryRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>
+ * search-solutions.net implementation of a QueryComponent to allow for
+ * server-side query templates.
+ * </p>
+ * <p/>
+ * <b>This API is experimental and may change in the future.</b>
+ * 
+ * @since solr 4.7
+ * @author Paul
+ * 
+ */
+public class SsqQueryComponent extends QueryComponent {
+	public static final String SSQ_DELIM = ".";
+	public static final String SSQ_PREFIX = "ssq";
+	public static final String SSQ_QUERY = "query";
+	public static final String SSQ_QUERYSTRING = "querystring";
+	public static final String SSQ_PARAM = "param";
+	public static final String SSQ_PARAM_DFT = "qq";
+	public static final String SSQ_APPLIED_SUFFIX = SSQ_DELIM + "applied";
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(SsqQueryComponent.class);
+
+	@Override
+	public void prepare(ResponseBuilder rb) throws IOException {
+		boolean isReplaced = false;
+
+		// modify query request
+		isReplaced = modifyQueryRequest(rb);
+
+		if (isReplaced)
+			LOG.debug("Modified query request:" + rb.req.getParams());
+
+		// perform the standard prepare method
+		super.prepare(rb);
+
+		// Restore the query parameters
+		if (isReplaced)
+			restoreQueryRequest(rb);
+	}
+
+	/**
+	 * Modify the query parameters
+	 */
+	private boolean modifyQueryRequest(ResponseBuilder rb) throws IOException {
+
+		SolrQueryRequest req = rb.req;
+		SolrParams params = req.getParams();
+
+		// check whether server side queries is active for this request
+		if (!params.getBool(SSQ_PREFIX, false))
+			return false;
+
+		// get parameters to use
+		String ssqQuery = params.get(SSQ_PREFIX.concat(SSQ_DELIM).concat(
+				SSQ_QUERY));
+		String ssqParam = params.get(
+				SSQ_PREFIX.concat(SSQ_DELIM).concat(SSQ_PARAM), SSQ_PARAM_DFT);
+
+		// when ssqQuery or ssqParam is not set, don't modify
+		if (ssqQuery == null || ssqQuery.isEmpty() || ssqParam.isEmpty())
+			return false;
+
+		// Get original value for ssqParam and return when already set
+		String ssqParamVal = params.get(ssqParam);
+		if (ssqParamVal != null && !ssqParamVal.isEmpty())
+			return false;
+
+		// Get original query string value
+		String origQueryString = rb.getQueryString();
+		String origQVal = req.getOriginalParams().get(CommonParams.Q);
+
+		// Retrieve value to use as query-term; when empty, use q.alt
+		String qVal = origQVal;
+		if (qVal == null || qVal.isEmpty()) {
+			String alt_q = params.get(DisMaxParams.ALTQ);
+			if (alt_q != null && !alt_q.isEmpty()) {
+				qVal = alt_q;
+			}
+		}
+
+		// Get value for ssqQuery
+		String ssqQueryVal = params.get(SSQ_PREFIX.concat(SSQ_DELIM)
+				.concat(SSQ_QUERY).concat(SSQ_DELIM).concat(ssqQuery));
+		// When value not found, assume that ssqQuery is the query to execute
+		// per default
+		if (ssqQueryVal == null || ssqQueryVal.isEmpty())
+			ssqQueryVal = ssqQuery;
+
+		// Perform replacement
+		ModifiableSolrParams mparams = new ModifiableSolrParams();
+
+		// Set flag to indicate that replacement is performed
+		mparams.set(SSQ_PREFIX.concat(SSQ_APPLIED_SUFFIX),
+				Boolean.toString(true));
+
+		// Store original querystring when <> q
+		if (origQVal != null && !origQVal.equals(origQueryString))
+			mparams.set(SSQ_PREFIX.concat(SSQ_DELIM).concat(SSQ_QUERYSTRING)
+					.concat(SSQ_APPLIED_SUFFIX), origQueryString);
+
+		// Perform the switch (qVal --> ssqParam)
+		mparams.set(ssqParam, qVal);
+		mparams.set(
+				SSQ_PREFIX.concat(SSQ_DELIM).concat(SSQ_QUERY)
+						.concat(SSQ_APPLIED_SUFFIX), ssqQueryVal);
+
+		// set the extra parameters
+		req.setParams(SolrParams.wrapAppended(req.getParams(), mparams));
+
+		// set queryString to query
+		rb.setQueryString(ssqQueryVal);
+
+		return true;
+	}
+
+	/**
+	 * Restore the query parameters Restore queryString and reset original q (to
+	 * ensure further components will be processed correctly)
+	 */
+	private boolean restoreQueryRequest(ResponseBuilder rb) {
+
+		SolrQueryRequest req = rb.req;
+		SolrParams params = req.getParams();
+
+		// check whether server side queries is active for this request
+		if (!params.getBool(SSQ_PREFIX, false))
+			return false;
+
+		// check whether values where replaced
+		if (!params.getBool(SSQ_PREFIX.concat(SSQ_APPLIED_SUFFIX), false))
+			return false;
+
+		// retrieve original queryString and q
+		String queryString = params.get(SSQ_PREFIX.concat(SSQ_DELIM)
+				.concat(SSQ_QUERYSTRING).concat(SSQ_APPLIED_SUFFIX));
+		boolean useStoredQueryString = (queryString != null && queryString
+				.isEmpty());
+
+		// remove flag that was possibly set to store original queryString
+		if (!useStoredQueryString) {
+			ModifiableSolrParams mparams = new ModifiableSolrParams(params);
+			mparams.remove(SSQ_PREFIX.concat(SSQ_DELIM).concat(SSQ_QUERYSTRING)
+					.concat(SSQ_APPLIED_SUFFIX));
+			req.setParams(mparams);
+		} else {
+			queryString = req.getOriginalParams().get(CommonParams.Q);
+		}
+
+		// set queryString to original query
+		rb.setQueryString(queryString);
+
+		return true;
+	}
+
+}
---
+++ src/test/java/net/sr_sl/solr/solr_ssq/SsqQueryComponentTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2015 search-solutions.net
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package net.sr_sl.solr.solr_ssq;
+
+import org.apache.solr.SolrTestCaseJ4;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Tests some basic Ssq functionality of Solr 
+ */
+public class SsqQueryComponentTest extends SolrTestCaseJ4 {
+
+	private static final String HEADER_XPATH = "/response/lst[@name='responseHeader']";
+
+	public String getCoreName() {
+		return "basic";
+	}
+
+	@BeforeClass
+	public static void beforeTests() throws Exception {
+		initCore("solr/ssq-solrconfig.xml", "solr/ssq-schema.xml");
+	    lrf.args.put("wt","xml");
+	    lrf.args.put("echoParams", "all");
+	    lrf.args.put("debugQuery", "on");
+	}
+
+	@Test
+	public void testSsqParams() {
+		
+		lrf.args.put("ssq","off");
+		assertQ(req("foo"), HEADER_XPATH
+				+ "/lst[@name='params']/str[@name='ssq'][.='off']");
+		assertQ(req("foo"), HEADER_XPATH
+				+ "/lst[@name='params']/str[@name='ssq.query.q1']");
+	}
+
+	@Test
+	public void testSsqProcessing() {
+		
+		assertU(adoc("id", "1001", "val1_s", "ABC"));
+		assertU(adoc("id", "1002", "val2_s", "ABC"));
+		assertU(adoc("id", "1003", "val3_s", "ABC"));
+		assertU(commit());
+		
+		assertQ("Test default query (with ssq off)", req("ABC"), "//*[@numFound='3']");
+
+		lrf.args.put("ssq", "on");
+		assertQ("Test default ssq.query (q1)", req("ABC"), "//*[@numFound='1']"
+	            ,"//result/doc[1]/str[@name='id'][.='1001']");
+		
+		lrf.args.put("ssq.query", "{!field f=val3_s v=$qq}");
+		assertQ("Test override ssq.query", req("ABC"), "//*[@numFound='1']"
+	            ,"//result/doc[1]/str[@name='id'][.='1003']");
+		
+		lrf.args.put("ssq.query", "q1");
+		assertQ("Test query q1", req("ABC"), "//*[@numFound='1']"
+	            ,"//result/doc[1]/str[@name='id'][.='1001']");
+		
+		lrf.args.put("ssq.query", "q2");
+		assertQ("Test query q2", req("ABC"), "//*[@numFound='1']"
+	            ,"//result/doc[1]/str[@name='id'][.='1002']");
+
+		lrf.args.remove("ssq");
+		
+	}
+
+}
